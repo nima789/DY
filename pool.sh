02e47e8fe920b14aac7bb2196336a7cdb4f866d6
nima789 <77829771+nima789@users.noreply.github.com>
2021-07-22 19:47:27 +0800

    Update pool.sh

--- pool.sh
+++ pool.sh
@@ -1,9 +1,16 @@
+POOL=/data/data/com.termux/files/home
+
 pkg update -y
 
 pkg install -y git curl wget
 
-apt get update
+git clone -b pool https://github.com/nima789/DY.git &{POOL}/pool
 
-apt-get install golang
+wget -P &{POOL}/pool/assets https://www.huahuayu.ml/pool/GeoLite2-City.mmdb
 
-git clone -b v0.7.3 https://github.com/Sansui233/proxypool.git && cd proxypool
+cd &{POOL}/pool
+
+nohup ./proxypool -c config.yaml
+
+echo -e '请打开pool文件夹复制下面代码运行'
+echo -e 'nohup ./proxypool -c config.yaml'
