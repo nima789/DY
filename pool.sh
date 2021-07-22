eb319db2e46eb9ff101cc253bb729b72b9edfd10
nima789 <77829771+nima789@users.noreply.github.com>
2021-07-22 20:10:46 +0800

    Update pool.sh

--- pool.sh
+++ pool.sh
@@ -1,18 +1,28 @@
 POOL=/data/data/com.termux/files/home
 
-pkg update -y
+function Installation() {
+   Update
+   ProjectDeployment
+   Tip
+}
+function Update() {
+   pkg update -y
 
-pkg install -y git curl wget
+   pkg install -y git curl wget
+}
+function ProjectDeployment() {
+   git clone -b pool https://github.com/nima789/DY.git &POOL/pool
 
-git clone -b pool https://github.com/nima789/DY.git &{POOL}/pool
+   wget -P &POOL/pool/assets https://www.huahuayu.ml/pool/GeoLite2-City.mmdb
 
-wget -P &{POOL}/pool/assets https://www.huahuayu.ml/pool/GeoLite2-City.mmdb
+   wget -P &POOL/pool https://www.huahuayu.ml/pool/proxypool
 
-wget -P &{POOL}/pool https://www.huahuayu.ml/pool/proxypool
-
-chmod 700 &{POOL}/pool
+   chmod 700 &POOL/pool
  
-chmod 700 &{POOL}/pool/proxypool
-
-echo -e '请打开pool文件夹复制下面代码运行'
-echo -e 'nohup ./proxypool -c config.yaml'
+   chmod 700 &POOL/pool/proxypool
+}
+function Tip() {
+   echo -e '请打开pool文件夹复制下面代码运行'
+   echo -e 'nohup ./proxypool -c config.yaml'
+}
+Installation
