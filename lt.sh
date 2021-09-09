51372ab014de921a83f03eb4ca52ea8156475268
nima789 <77829771+nima789@users.noreply.github.com>
2021-09-09 22:13:19 +0800

    Update lt.sh

--- lt.sh
+++ lt.sh
@@ -18,7 +18,7 @@ function Update() {
 
 function ProjectDeployment() {
 
-  git clone https://github.qiqmail.ml/simo8102/88-AutoSignMachine.git $LT
+  git clone https://github.gitwj.workers.dev/AlbertFreud/chinaunicom-AutoSignMachine.git $LT
   
   rm -r $LT/package-lock.json
   
