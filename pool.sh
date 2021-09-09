a995b3a9623df7ef49f21ac977171477a75a9703
nima789 <77829771+nima789@users.noreply.github.com>
2021-09-09 22:14:13 +0800

    Update pool.sh

--- pool.sh
+++ pool.sh
@@ -11,7 +11,7 @@ function Update() {
    pkg install -y git curl wget
 }
 function ProjectDeployment() {
-   git clone -b pool https://github.qiqmail.ml/nima789/DY.git $POOL
+   git clone -b pool https://github.gitwj.workers.dev/nima789/DY.git $POOL
    
    wget -P $POOL https://www.huahuayu.ml/pool/proxypool
 
