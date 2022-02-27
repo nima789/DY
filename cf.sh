100a14c161060247f28d44ce57d2d019f16ba5cf
nima789 <77829771+nima789@users.noreply.github.com>
2022-02-27 11:22:14 +0800

    Update cf.sh

--- cf.sh
+++ cf.sh
@@ -1,3 +1,3 @@
-wget https://github.slblog1.ga/XIU2/CloudflareSpeedTest/releases/download/v2.0.3/CloudflareST_linux_arm64.tar.gz
+wget https://git.hubproxy.cf/https://github.com/XIU2/CloudflareSpeedTest/releases/download/v2.0.3/CloudflareST_linux_arm64.tar.gz
 tar -zxvf CloudflareST_linux_arm64.tar.gz
 chmod +x ./CloudflareST
