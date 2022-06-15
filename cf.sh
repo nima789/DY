9f0b081133b04a9952564e472e1ac15a3a5883dd
nima789 <77829771+nima789@users.noreply.github.com>
2022-06-15 11:20:29 +0800

    Delete cf.sh

--- cf.sh
+++
@@ -1,5 +0,0 @@
-rm -rf /data/data/com.termux/files/home/cf
-mkdir cf
-cd cf && wget https://git.hubproxy.cf/https://github.com/XIU2/CloudflareSpeedTest/releases/download/v2.0.3/CloudflareST_linux_arm64.tar.gz
-tar -zxvf CloudflareST_linux_arm64.tar.gz
-chmod +x ./CloudflareST
