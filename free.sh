055419e28eb9883641cd711d669caa4bf62ae9a7
nima789 <77829771+nima789@users.noreply.github.com>
2022-07-27 22:31:06 +0800

    Update free.sh

--- free.sh
+++ free.sh
@@ -11,7 +11,8 @@ function Install() {
 function Down() {
   mkdir /root/freenom
   wget -O /root/freenom/free.py https://raw.githubusercontent.com/Oreomeow/freenom-py/main/FNplus.py
-  wget -O /root/freenom/requirements.txt https://raw.githubusercontent.com/Oreomeow/freenom-py/main/requirements.txt
+  wget -P /root/freenom https://raw.githubusercontent.com/Oreomeow/freenom-py/main/requirements.txt
+  wget -P /root/freenom https://raw.githubusercontent.com/whyour/qinglong/master/sample/notify.py
   pip3 install -r /root/freenom/requirements.txt
 }
 
