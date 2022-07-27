baec007ee66234351868dc4c8969b72db26fa292
nima789 <77829771+nima789@users.noreply.github.com>
2022-07-27 22:08:50 +0800

    Update free.sh

--- free.sh
+++ free.sh
@@ -1,5 +1,18 @@
-apt install python3 python3-pip -y
-mkdir /root/freenom
-wget -O /root/freenom/free.py https://raw.githubusercontent.com/Oreomeow/freenom-py/main/FNplus.py
-wget -O /root/freenom/requirements.txt https://raw.githubusercontent.com/Oreomeow/freenom-py/main/requirements.txt
-pip3 install -r /root/freenom/requirements.txt
+function Implement() {
+  Install
+  Down
+}
+
+function Install() {
+  apt update
+  apt install -y python3 python3-pip
+}
+
+function Down() {
+  mkdir /root/freenom
+  wget -O /root/freenom/free.py https://raw.githubusercontent.com/Oreomeow/freenom-py/main/FNplus.py
+  wget -O /root/freenom/requirements.txt https://raw.githubusercontent.com/Oreomeow/freenom-py/main/requirements.txt
+  pip3 install -r /root/freenom/requirements.txt
+}
+
+Implement
