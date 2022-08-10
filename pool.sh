4b070de8dbc52d86862a3586805124892a43e941
nima789 <77829771+nima789@users.noreply.github.com>
2022-08-10 13:09:17 +0800

    Update pool.sh

--- pool.sh
+++ pool.sh
@@ -17,8 +17,8 @@ function Installation() {
 function ProjectDeployment() {
    rm -rf $POOL
    git clone -b pool https://github.hhymail.ml/nima789/DY.git $POOL 
-   wget -O $POOL/proxypool https://xzp.xiezhang.ml/api/raw/?path=/proxypool/proxypool
-   wget -O $POOL/assets/GeoLite2-City.mmdb https://xzp.xiezhang.ml/api/raw/?path=/proxypool/GeoLite2-City.mmdb
+   wget -O $POOL/proxypool https://xzpan.xiezhang.ml/proxypool/proxypool
+   wget -O $POOL/assets/GeoLite2-City.mmdb https://xzpan.xiezhang.ml/proxypool/GeoLite2-City.mmdb
    chmod 700 $POOL
    chmod 700 $POOL/proxypool
 } 
