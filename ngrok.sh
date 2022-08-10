4107b0a05138517841dc1c397ddb10ea7debbb91
nima789 <77829771+nima789@users.noreply.github.com>
2022-08-10 15:46:20 +0800

    Update ngrok.sh

--- ngrok.sh
+++ ngrok.sh
@@ -2,3 +2,5 @@ wget -O ngrok https://xzpan.xiezhang.ml/%E6%96%87%E4%BB%B6/ngrok-2.2.8-arm64
 chmod 777 ngrok
 mv ngrok $PREFIX/bin/
 ngrok -v
+echo -e '添加authtoken格式'
+echo -e 'ngrok authtoken xxxxxxx'
