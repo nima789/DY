57d0ab7dfc19d4ea1fdd33d797543c5e3d984262
nima789 <77829771+nima789@users.noreply.github.com>
2022-08-14 20:58:16 +0800

    Update cf.sh

--- cf.sh
+++ cf.sh
@@ -1,6 +1,9 @@
-rm -rf /data/data/com.termux/files/home/cf
-mkdir cf
+rm -rf $HOME/cf
+mkdir $HOME/cf
+git clone https://git.hubproxy.cf/https://github.com/ip-scanner/cloudflare.git $HOME/cf/cfip
+cd $HOME/cf/cfip && cat *.txt >> all.txt
 wget -P /data/data/com.termux/files/home/cf https://git.hubproxy.cf/https://raw.githubusercontent.com/badafans/better-cloudflare-ip/master/shell/cf.sh
 cd cf && wget https://git.hubproxy.cf/https://github.com/XIU2/CloudflareSpeedTest/releases/download/v2.0.3/CloudflareST_linux_arm64.tar.gz
 tar -zxvf CloudflareST_linux_arm64.tar.gz
 chmod +x ./CloudflareST
+mv -f $HOME/cf/cfip/all.txt $HOME/cf/ip.txt
