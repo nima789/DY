3959e35006f1ab3d7171f5e74c0d2e65fdf2e00f
nima789 <77829771+nima789@users.noreply.github.com>
2022-08-16 22:37:20 +0800

    Update cf.sh

--- cf.sh
+++ cf.sh
@@ -1,9 +1,9 @@
 rm -rf $HOME/cf
 mkdir $HOME/cf
-git clone https://xzgh.run.goorm.io/https://github.com/ip-scanner/cloudflare.git $HOME/cf/cfip
+git clone https://git.hubproxy.cf/https://github.com/ip-scanner/cloudflare.git $HOME/cf/cfip
 cd $HOME/cf/cfip && cat *.txt >> all.txt
-wget -P /data/data/com.termux/files/home/cf https://xzgh.run.goorm.io/https://raw.githubusercontent.com/badafans/better-cloudflare-ip/master/shell/cf.sh
-cd $HOME/cf && wget https://xzgh.run.goorm.io/https://github.com/XIU2/CloudflareSpeedTest/releases/download/v2.0.3/CloudflareST_linux_arm64.tar.gz
+wget -P /data/data/com.termux/files/home/cf https://git.hubproxy.cf/https://raw.githubusercontent.com/badafans/better-cloudflare-ip/master/shell/cf.sh
+cd $HOME/cf && wget https://git.hubproxy.cf/https://github.com/XIU2/CloudflareSpeedTest/releases/download/v2.0.3/CloudflareST_linux_arm64.tar.gz
 tar -zxvf CloudflareST_linux_arm64.tar.gz
 chmod +x ./CloudflareST
 mv -f $HOME/cf/cfip/all.txt $HOME/cf/ip.txt
