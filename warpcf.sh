85681e811388bd0b2bc4d2a5a5160992dc486cf6
nima789 <77829771+nima789@users.noreply.github.com>
2022-12-13 16:54:32 +0800

    Create warpcf.sh

---
+++ warpcf.sh
@@ -0,0 +1,7 @@
+mkdir $HOME/cf
+cd $HOME/cf
+wget https://gh.hubproxy.cf/https://raw.githubusercontent.com/nima789/DY/file/cf/cfst_hosts.sh
+wget https://gh.hubproxy.cf/https://github.com/nima789/DY/raw/file/cf/CloudflareST
+wget https://gh.hubproxy.cf/https://raw.githubusercontent.com/nima789/DY/file/ip.txt
+chmod +x ./CloudflareST
+chmod +x ./cfst_hosts.sh
