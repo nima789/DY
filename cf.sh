2845e0af4cfe52bdbcd8f34310a35ddd65523a2e
nima789 <77829771+nima789@users.noreply.github.com>
2023-01-24 17:03:58 +0800

    Add files via upload

--- cf.sh
+++ cf.sh
@@ -1,9 +1,45 @@
-rm -rf $HOME/cf
-mkdir $HOME/cf
-git clone https://gh.hubproxy.cf/https://github.com/ip-scanner/cloudflare.git $HOME/cf/cfip
-cd $HOME/cf/cfip && cat *.txt >> all.txt
-wget -P /data/data/com.termux/files/home/cf https://gh.hubproxy.cf/https://raw.githubusercontent.com/badafans/better-cloudflare-ip/master/shell/cf.sh
-cd $HOME/cf && wget https://gh.hubproxy.cf/https://github.com/XIU2/CloudflareSpeedTest/releases/download/v2.0.3/CloudflareST_linux_arm64.tar.gz
-tar -zxvf CloudflareST_linux_arm64.tar.gz
-chmod +x ./CloudflareST
-mv -f $HOME/cf/cfip/all.txt $HOME/cf/ip.txt
+function Install() {
+  echo -e "Cloudflare代理ip脚本"
+  echo -e "1. 安装CloudflareST"
+  echo -e "2. ip-daily分支"
+  echo -e "3. ip-master分支"
+  echo -e "0. 退出"
+  read -p "请输入数字：" num
+  case "$num" in
+  1)CloudflareST;;
+  2)Daily;;
+  3)Master;;
+  *)exit;
+  esac
+}
+
+function CloudflareST() {
+  rm -rf $HOME/cf
+  mkdir $HOME/cf
+  wget -P $HOME/cf https://gh.hubproxy.cf/https://raw.githubusercontent.com/badafans/better-cloudflare-ip/master/shell/cf.sh
+  wget -P $HOME/cf https://gh.hubproxy.cf/https://github.com/XIU2/CloudflareSpeedTest/releases/download/v2.0.3/CloudflareST_linux_arm64.tar.gz
+cd $HOME/cf && tar -zxvf CloudflareST_linux_arm64.tar.gz
+  chmod +x ./CloudflareST
+  echo -e "CloudflareST 安装完成"
+  Install
+}
+
+function Daily() {
+  rm -rf $HOME/cf/cfip
+  git clone -b daily https://gh.hubproxy.cf/https://github.com/ip-scanner/cloudflare.git $HOME/cf/cfip
+  cd $HOME/cf/cfip && cat *.txt >> all.txt
+  cp $HOME/cf/cfip/all.txt $HOME/cf/ip.txt
+  echo -e "ip-daily分支 安装完成"
+  echo -e "cd cf && ./CloudflareST 运行"
+}
+
+function Master() {
+  rm -rf $HOME/cf/cfip
+  git clone -b master https://gh.hubproxy.cf/https://github.com/ip-scanner/cloudflare.git $HOME/cf/cfip
+  cd $HOME/cf/cfip && cat *.txt >> all.txt
+  cp $HOME/cf/cfip/all.txt $HOME/cf/ip.txt
+  echo -e "ip-master分支 安装完成"
+  echo -e "cd cf && ./CloudflareST 运行"
+}
+
+Install
