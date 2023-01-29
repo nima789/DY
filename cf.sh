7a8197505130a54f8faeb21c9dfade0bd26cb3fd
nima789 <77829771+nima789@users.noreply.github.com>
2023-01-29 21:02:26 +0800

    Update cf.sh

--- cf.sh
+++ cf.sh
@@ -17,7 +17,7 @@ function CloudflareST() {
   rm -rf $HOME/cf
   mkdir $HOME/cf
   wget -P $HOME/cf https://gh.hubproxy.cf/https://raw.githubusercontent.com/badafans/better-cloudflare-ip/master/shell/cf.sh
-  wget -P $HOME/cf https://gh.hubproxy.cf/https://github.com/XIU2/CloudflareSpeedTest/releases/download/v2.0.3/CloudflareST_linux_arm64.tar.gz
+  wget -P $HOME/cf https://gh.hubproxy.cf/https://github.com/XIU2/CloudflareSpeedTest/releases/download/v2.1.0/CloudflareST_linux_arm64.tar.gz
   cd $HOME/cf && tar -zxvf CloudflareST_linux_arm64.tar.gz
   chmod +x ./CloudflareST
   echo -e "CloudflareST 安装完成"
